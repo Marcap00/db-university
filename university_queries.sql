90f1d44e7a9502b0cea93ef58c6d8f9ca1dcbd2a
Marcap00 <cappellatomarco@yahoo.com>
2024-10-16 15:59:21 +0200

    third queries completed

--- university_queries.sql
+++ university_queries.sql
@@ -10,6 +10,15 @@ WHERE `cfu` > 10;
 
 
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
+SELECT *
+FROM `students`
+WHERE YEAR(CURDATE()) - YEAR(`date_of_birth`) > 30; -- With CURDATE()
+-- ORDER BY `date_of_birth` DESC;
+SELECT *
+FROM `students`
+WHERE YEAR(NOW()) - YEAR(`date_of_birth`) > 30; -- With NOW()
+-- ORDER BY `date_of_birth` DESC;
+
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di
 -- laurea (286)
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del
