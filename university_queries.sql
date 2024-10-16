e2e1651f2de79fd5e8804dede2c43b68e2b60c4e
Marcap00 <cappellatomarco@yahoo.com>
2024-10-16 16:04:37 +0200

    fourth query completed

--- university_queries.sql
+++ university_queries.sql
@@ -14,13 +14,17 @@ SELECT *
 FROM `students`
 WHERE YEAR(CURDATE()) - YEAR(`date_of_birth`) > 30; -- With CURDATE()
 -- ORDER BY `date_of_birth` DESC;
+
 SELECT *
 FROM `students`
 WHERE YEAR(NOW()) - YEAR(`date_of_birth`) > 30; -- With NOW()
 -- ORDER BY `date_of_birth` DESC;
 
--- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di
--- laurea (286)
+-- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
+SELECT * 
+FROM `courses`
+WHERE `period` = 'I semestre' AND `year` = 1;
+
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del
 -- 20/06/2020 (21)
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
